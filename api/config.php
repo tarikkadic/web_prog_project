02b2e38c691153d629fe8bbd0508a7594457ee6d
tarikkadic <tarik.kadic@stu.ibu.edu.ba>
2021-04-07 19:37:41 +0200

    dao setup

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config{
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "newsnow";
+  const DB_PASSWORD = "newsnow";
+  const DB_SCHEME = "web_prog_project";
+
+}
+?>
---
+++ api/dao/UserDao.class.php
@@ -0,0 +1,9 @@
+<?php
+class UserDao extends BaseDao{
+
+  public function get_user_by_email($email){
+    
+  }
+
+}
+ ?>
