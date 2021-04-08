39aafefea8f731112cd12998f116fe13be3e7965
tarikkadic <tarik.kadic@stu.ibu.edu.ba>
2021-04-08 19:53:37 +0200

    update user added

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -20,7 +20,16 @@ class UserDao extends BaseDao{
   }
 
   public function update_user($id, $user){
+    $query = "UPDATE users SET ";
+    foreach ($user as $name => $value){
+      $query .= $name ."= :". $name. ", ";
+    }
 
+    $query = substr($query, 0 , -2);
+    $query .= " WHERE id = :id";
+    $stmt = $this->connection->prepare($query);
+    $user["id"] = $id;
+    $stmt->execute($user);
   }
 
 }
--- api/test.php
+++ api/test.php
@@ -6,15 +6,15 @@ $user_dao = new UserDao();
 //$user = $user_dao->get_user_by_id(1);
 
 $user1 = [
-  "username" => "user1",
-  "email" => "user1@gmail.com",
-  "password" => "1234",
-  "account_id" => 1
+  "username" => "user123",
+  "email" => "user123@gmail.com",
+  "password" => "12345",
 ];
 
-$user = $user_dao->add_user($user1);
+
+$user = $user_dao->update_user(4, $user1);
 
 //get_user_by_email("tarik.kadic@gmail.com");
 
-print_r($user);
+//print_r($user);
 ?>
