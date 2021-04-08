8393cc1d361e1e295c9176acd0fe79f3a2615446
tarikkadic <tarik.kadic@stu.ibu.edu.ba>
2021-04-08 20:52:36 +0200

    add generic insert

--- api/test.php
+++ api/test.php
@@ -1,18 +1,24 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once dirname(__FILE__)."/dao/UserDao.class.php";
+
 
 $user_dao = new UserDao();
 
 //$user = $user_dao->get_user_by_id(1);
 
 $user1 = [
-  "password" => "password123"
+  "username" => "newest user1",
+  "email" => "newestuser1@gmail.com",
+  "account_id" => 1,
+  "password" => "sifra123"
 ];
 
-
-$user = $user_dao->update_user_by_email("user123@gmail.com", $user1);
+$user = $user_dao->add_user($user1);
 
 //get_user_by_email("tarik.kadic@gmail.com");
 
-//print_r($user);
+print_r($user);
 ?>
