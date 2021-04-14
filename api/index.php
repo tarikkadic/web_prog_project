81bb0ce3b31f017fa7e012b7b25ea404a630d721
tarikkadic <tarik.kadic@stu.ibu.edu.ba>
2021-04-14 02:10:51 +0200

    flight routes optimization

--- api/index.php
+++ api/index.php
@@ -14,22 +14,16 @@ Flight::route('/', function(){
 });
 
 Flight::route('GET /users', function(){
-    $users = Flight::userDao()->get_all(0, 2);
-    Flight::json($users);
+    Flight::json(Flight::userDao()->get_all(0, 2));
 });
 
 Flight::route('GET /users/@id', function($id){
-    $users = Flight::userDao()->get_user_by_id($id);
-    Flight::json($users);
+    Flight::json(Flight::userDao()->get_user_by_id($id));
 });
 
 Flight::route('POST /users', function(){
-    $request = Flight::request();
-    $data = $request->data->getData();
-
-    $user = Flight::userDao()->add($data);
-
-    Flight::json($user);
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::userDao()->add($data));
 });
 
 Flight::route('PUT /users/@id', function($id){
